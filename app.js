6dbd7b8c667f709fdb0098b688f1dbe427ac2f01
James Nunoo <jamesnunoo@Jamess-MacBook-Pro.local>
2020-06-06 17:08:56 +0300

    set up app for Heroku

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const express = require("express");
 const request = require("request");
-const app = express();
 const hbs = require("hbs");
 const geoCode = require("./src/utils/geoCode");
 const forecast = require("./src/utils/forecast");
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 //defining paths for express config
 const publicDirectoryPath = path.join(__dirname, "./public");
@@ -97,6 +99,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(port, () => {
+  console.log("Server started on port " + port);
 });
--- public/js/app.js
+++ public/js/app.js
@@ -11,16 +11,14 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading....";
   messageTwo.textContent = " ";
 
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = data.loc;
-          messageTwo.textContent = data.forecast.currentWeather;
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.loc;
+        messageTwo.textContent = data.forecast.currentWeather;
+      }
+    });
+  });
 });
